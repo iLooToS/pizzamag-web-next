0bbe1debdd5ae90bce01f08ecf57f8a5242d82b4
iLooToS <rhymudienullte@gmail.com>
2024-10-01 20:30:31 +0300

    header-component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Button variant={"outline"}>Корзина</Button>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
