d1162f6ac5304f94a05f94ca68d47a9688ee67d7
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-10-06 08:39:41 +0300

    checkbox-filters-group-search-input

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -55,6 +55,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
           { text: "Солёные огурчики", value: "4" },
           { text: "Красный лук", value: "5" },
           { text: "Томаты", value: "6" },
+          { text: "Лук", value: "7" },
         ]}
       />
     </div>
