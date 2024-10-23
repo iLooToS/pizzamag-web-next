0ea83c8c3c11746974e73e0f0c498a402b65a071
iLooToS <rhymudienullte@gmail.com>
2024-10-24 01:04:29 +0300

    router-group

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+
+import { Header } from "@/components/shared/header";
+
+export const metadata: Metadata = {
+  title: "Pizza Mag | Главная",
+  description: "Самая вкусная пицца во вселенной",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      {children}
+    </main>
+  );
+}
