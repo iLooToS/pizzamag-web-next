6ee0e27edd8f86a68d9f4bf7e19de47b3eb5a86c
iLooToS <rhymudienullte@gmail.com>
2024-10-26 21:50:35 +0300

    pizza-modal-window-and-imports

---
+++ shared/components/shared/choose-pizza-form.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { cn } from "@/shared/lib/utils";
+import React from "react";
+import { Title } from "./title";
+import { Button } from "../ui";
+import { PizzaImage } from "./pizza-image";
+import { GroupVariants } from "./group-variants";
+import {
+  PizzaSize,
+  pizzaSizes,
+  PizzaType,
+  pizzaTypes,
+} from "@/shared/constants/pizza";
+import { Ingredient } from "@prisma/client";
+import { IngredientItem } from "./ingredient-item";
+import { useSet } from "react-use";
+
+interface Props {
+  imageUrl: string;
+  name: string;
+  ingredients: Ingredient[];
+  items?: any[];
+  onClickAdd?: VoidFunction;
+  className?: string;
+}
+
+export const ChoosePizzaForm: React.FC<Props> = ({
+  name,
+  items,
+  imageUrl,
+  ingredients,
+  onClickAdd,
+  className,
+}) => {
+  const [size, setSize] = React.useState<PizzaSize>(20);
+  const [type, setType] = React.useState<PizzaType>(1);
+
+  const [selectedIngredients, { toggle: addIngredient }] = useSet(
+    new Set<number>([])
+  );
+
+  const textDetaills = "30 см, традиционное тесто 30";
+  const totalPrice = 350;
+
+  return (
+    <div className={cn(className, "flex flex-1")}>
+      <PizzaImage imageUrl={imageUrl} size={size} productName={name} />
+
+      <div className="w-[490px] bg-[#f7f6f5] p-7">
+        <Title text={name} size="md" className="font-extrabold mb-1" />
+
+        <p className="text-gray-400">{textDetaills}</p>
+
+        <div className="flex flex-col gap-4 mt-5">
+          <GroupVariants
+            items={pizzaSizes}
+            value={String(size)}
+            onClick={(value) => setSize(Number(value) as PizzaSize)}
+          />
+
+          <GroupVariants
+            items={pizzaTypes}
+            value={String(type)}
+            onClick={(value) => setType(Number(value) as PizzaType)}
+          />
+        </div>
+
+        <div className="bg-gray-50 p-5 rounded-md h-[420px] overflow-auto scrollbar mt-5">
+          <div className="grid grid-cols-3 gap-3">
+            {ingredients.map((ingredient) => (
+              <IngredientItem
+                key={ingredient.id}
+                imageUrl={ingredient.imageUrl}
+                name={ingredient.name}
+                price={ingredient.price}
+                active={selectedIngredients.has(ingredient.id)}
+                onClick={() => addIngredient(ingredient.id)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mr-10 mt-5">
+          Добавить в корзину за {totalPrice} ₽
+        </Button>
+      </div>
+    </div>
+  );
+};
---
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cn } from "@/shared/lib/utils";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ChooseProductForm } from "../choose-product-form";
+import { ProductWithRelations } from "@/@types/prisma";
+import { ChoosePizzaForm } from '../choose-pizza-form';
+import { Dialog } from '../../ui';
+import { DialogContent } from '../../ui/dialog';
+
+interface Props {
+  product: ProductWithRelations;
+  className?: string;
+}
+
+export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
+  const router = useRouter();
+  const isPizzaForm = Boolean(product.items[0].pizzaType);
+
+  return (
+    <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
+      <DialogContent
+        className={cn(
+          className,
+          "p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden"
+        )}
+      >
+        {isPizzaForm ? (
+          <ChoosePizzaForm imageUrl={product.imageUrl} name={product.name} ingredients={product.ingredients}/>
+        ) : (
+          <ChooseProductForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+          />
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
