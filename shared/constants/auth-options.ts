6985aad7b4fb6cef584c15ff876570d16c9dbd23
iLooToS <rhymudienullte@gmail.com>
2024-11-13 01:52:21 +0300

    stories

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -1,4 +1,5 @@
 import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/prisma/prisma-client";
@@ -7,6 +8,10 @@ import { UserRole } from "@prisma/client";
 
 export const authOptions: AuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
