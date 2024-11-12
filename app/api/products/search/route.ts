63de1c23d7f01a6729169ce661bd11550e58b10c
iLooToS <rhymudienullte@gmail.com>
2024-11-12 09:36:38 +0300

    authorization module

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const query = req.nextUrl.searchParams.get("query") || "";
-  console.log(query);
 
   const products = await prisma.product.findMany({
     where: {
